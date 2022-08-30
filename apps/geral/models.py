5aa00fceab8d001c2e41b0e5c944abbf67808784
Francisco <fandrefh@gmail.com>
2022-08-29 21:33:22 -0300

    Django admin

--- apps/geral/models.py
+++ apps/geral/models.py
@@ -12,4 +12,12 @@ class Categoria(models.Model):
     nome = models.CharField(verbose_name='Nome', max_length=50)
     tipo = models.CharField(verbose_name='Tipo', max_length=1, choices=TIPO_CHOICES)
     descricao = models.TextField(verbose_name='Descrição', blank=True, null=True)
+    usuario = models.ForeignKey(User, verbose_name='Usuário', on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        ordering = ['nome']
+
+    def __str__(self):
+        return self.nome
